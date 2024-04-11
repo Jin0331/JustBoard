8a3fbbbc4ee5159de5b44fc3a90a12cc1d6bdb23
Jin0331 <sempre813@naver.com>
2024-04-11 15:44:07 +0900

    Initial Commit

---
+++ YeogiApa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YeogiApa
+//
+//  Created by JinwooLee on 4/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
